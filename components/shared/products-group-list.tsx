f2a12c1e5631f1a6067d896782fd63a2c0cce901
Raman Andreyuk <romaandreyuk@gmail.com>
2025-09-23 14:14:21 +0200

    zustand for providing state into categories. intersection for checking active categoryId

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 
 import React from 'react';
+import {useIntersection} from "react-use";
 import {Title} from "@/components/shared/title";
 import { cn } from '@/lib/utils';
 import {ProductCard} from "@/components/shared/product-card";
+import {useCategoryStore} from "@/store/category";
+
 interface Props{
     title: string;
     items: any[];
@@ -13,7 +18,18 @@ interface Props{
 }
 
 export const ProductsGroupList: React.FC<Props> = ({ title, items, listClassName, categoryId,className}) => {
-    return <div className={className}>
+
+    const setActiveCategoryId = useCategoryStore((state)=>state.setActiveId)
+    const intersectionRef = React.useRef(null);
+    const intersection = useIntersection(intersectionRef,{threshold: 0.4})
+
+    React.useEffect(()=>{
+        if(intersection?.isIntersecting){
+            setActiveCategoryId(categoryId)
+        }
+    }, [categoryId,intersection?.isIntersecting, title])
+
+    return <div className={className} id={title} ref={intersectionRef}>
         <Title text={title} size="lg" className="font-extrabold mb-5"/>
         <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
             {items.map((product, index) => (
