2a3bee45d2fc9cab8aeda20f2c2d1281f0e4f3e3
Raman Andreyuk <romaandreyuk@gmail.com>
2025-09-28 17:09:42 +0200

    displaying categories from backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,58 @@
+import { Container } from '@/components/shared/container';
+import { Title } from '@/components/shared/title';
+import { TopBar } from '@/components/shared/top-bar';
+import { Filters } from '@/components/shared/filters';
+import { ProductsGroupList } from '@/components/shared/products-group-list';
+import { prisma } from '@/prisma/prisma-client';
 
-import {Container} from "@/components/shared/container";
-import {Title} from "@/components/shared/title";
-import {TopBar} from "@/components/shared/top-bar";
-import {Filters} from "@/components/shared/filters";
-import {ProductsGroupList} from "@/components/shared/products-group-list";
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        },
+      },
+    },
+  });
 
+  console.log(categories);
 
-export default function Home() {
   return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar categories={categories.filter((category) => category.products.length > 0)} />
 
-   <>
-       <Container className="mt-10">
-<Title text="Все пиццы"
-size="lg"
-className="font-extrabold"/>
-       </Container>
-          <TopBar/>
+      <Container className=" mt-10 pb-14">
+        <div className="flex gap-[80px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
 
-       <Container className=" mt-10 pb-14">
-<div className="flex gap-[80px]">
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products}
+                    />
+                  ),
+              )}
+            </div>
+          </div>
 
-    {/* Фильтрация */}
-<div className="w-[250px]">
-    <Filters/>
-</div>
-
-    {/* Список товаров */}
-    <div className="flex-1">
-        <div className="flex flex-col gap-16">
-            <ProductsGroupList title="Пиццы"
-items={[{id:1, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:2, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:3, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:4, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:5, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:6, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:7, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:8, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]}]}
-categoryId={1}/>
-            <ProductsGroupList title="Комбо"
-items={[{id:9, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:10, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:11, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:12, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:13, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:14, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:15, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]},{id:16, name:'Чизбурер-пицца', imageUrl: "https://media.dodostatic.net/image/r:292x292/0198bf4f806371f19d529f9e9e7dba36.avif", price:550, items: [{price:550}]}]}
-categoryId={2}/>
+          {/*  Фильтр ингридиентов */}
         </div>
-    </div>
-
-    {/*  Фильтр ингридиентов */}
-
-
-</div>
-       </Container>
-   </>
-
+      </Container>
+    </>
   );
 }
