23f488dfa0228d7eeb144185d7f1a2bf64833cbd
Raman Andreyuk <romaandreyuk@gmail.com>
2025-10-02 18:40:48 +0200

    creating Cart with items from backend, useCartStore zustand hook for using data in front

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -14,32 +14,56 @@ import { Button } from '@/shared/components/ui/button';
 import { ArrowRight } from 'lucide-react';
 import { CartDriwerItem } from '@/shared/components/shared/cart-driwer-item';
 import { getCartItemDetails } from '@/shared/lib/get-cart-item-details';
+import { useCartStore } from '@/shared/store/cart';
+import { PizzaSize, PizzaType } from '@/shared/constants/pizza';
 
 interface Props {
   className?: string;
 }
 
 export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({ children, className }) => {
+  // const [totalAmount, fetchCartItems, items] = useCartStore((state) => [
+  //   state.totalAmount,
+  //   state.fetchCartItems,
+  //   state.items,
+  // ]); в новых версиях не работает т к селектор возвращает новый массив на каждом рендере
+  const totalAmount = useCartStore((state) => state.totalAmount);
+  const fetchCartItems = useCartStore((state) => state.fetchCartItems);
+  const items = useCartStore((state) => state.items);
+
+  React.useEffect(() => {
+    fetchCartItems();
+  }, []);
+
   return (
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
         <SheetHeader>
           <SheetTitle>
-            В корзине <span className="font-bold">3 товара</span>
+            В корзине <span className="font-bold">{items.length} товара</span>
           </SheetTitle>
         </SheetHeader>
 
-        <CartDriwerItem
-          id={1}
-          imageUrl={
-            'https://media.dodostatic.net/image/r:292x292/0198da9ffdfe782bb8d982b10c5e6b43.avif'
-          }
-          details={getCartItemDetails(2, 30, [{ name: 'Цыпленок' }, { name: 'Сыр' }])}
-          name={'Чоризо Фреш'}
-          price={419}
-          quantity={1}
-        />
+        {items.map((item) => (
+          <CartDriwerItem
+            key={item.id}
+            id={item.id}
+            imageUrl={item.image_url}
+            details={
+              item.pizzaSize && item.pizzaType
+                ? getCartItemDetails(
+                    item.ingredients,
+                    item.pizzaType as PizzaType,
+                    item.pizzaSize as PizzaSize,
+                  )
+                : ''
+            }
+            name={item.name}
+            price={item.price}
+            quantity={item.quantity}
+          />
+        ))}
 
         <SheetFooter className="-mx-6 bg-white p-8">
           <div className="w-full">
@@ -48,7 +72,7 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({ children,
                 Итого
                 <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
               </span>
-              <span className="font-bold text-lg">500 Р</span>
+              <span className="font-bold text-lg">{totalAmount} Р</span>
             </div>
             <Link href="/cart">
               <Button type="submit" className="w-full h-12 text-base">
---
+++ shared/store/cart.ts
@@ -0,0 +1,51 @@
+'use client';
+
+import { create } from 'zustand';
+import { Api } from '@/shared/services/api-client';
+import { CartStateItem, getCartDetails } from '@/shared/lib/get-cart-details';
+
+
+
+export interface CartState {
+  loading: boolean;
+  error: boolean;
+  totalAmount: number;
+  items: CartStateItem[];
+
+  /* Получение товаров */
+  fetchCartItems: () => Promise<void>;
+
+  /*  Запрос на обновление количества товара */
+  updateItemQuantity: (id: number, quantity: number) => Promise<void>;
+
+  /* Запрс на добавление товара в корзину */
+  // TODO!!! Типизировать values
+  addCartItem: (values: any) => Promise<void>;
+
+  /* Запрс на удаление товара из корзины */
+  removeCartItem: (id: number) => Promise<void>;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+  items: [],
+  error: false,
+  loading: true,
+  totalAmount: 0,
+
+  fetchCartItems: async () => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.fetchCart();
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  removeCartItem: async (id: number) => {},
+  updateItemQuantity: async (id: number, quantity: number) => {},
+  addCartItem: async (values: any) => {},
+}));
