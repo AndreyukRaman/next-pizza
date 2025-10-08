f9051723e809e88b3b1af1945d6455d72da0babe
Raman Andreyuk <romaandreyuk@gmail.com>
2025-10-08 19:57:24 +0200

    wrapping address-input into <Controller> for using validation

--- shared/components/shared/checkout/checkout-address-form.tsx
+++ shared/components/shared/checkout/checkout-address-form.tsx
@@ -4,20 +4,34 @@ import React from 'react';
 import { WhiteBlock } from '@/shared/components/shared/white-block';
 import { FormTextarea } from '@/shared/components/shared/form/form-textarea';
 import { AddressInput } from '@/shared/components/shared/address-input';
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
+import { ErrorText } from '@/shared/components/shared/error-text';
 
 interface Props {
   className?: string;
 }
 
 export const CheckoutAddressForm: React.FC<Props> = ({ className }) => {
-  const { setValue } = useFormContext();
+  const { control } = useFormContext();
   return (
     <WhiteBlock title="3. Адрес доставки">
       <div className="flex flex-col gap-5">
         {/*<Input name="firstName" className="text-base" placeholder="Введите адрес" />*/}
 
-        <AddressInput onChange={(value) => setValue('address', value)} />
+        <Controller
+          name="address"
+          control={control}
+          rules={{ required: 'Адрес обязателен' }} // пример валидации
+          render={({ field, fieldState }) => (
+            <>
+              <AddressInput
+                value={field.value} // текущее значение поля
+                onChange={(value) => field.onChange(value)} // уведомляем RHF об изменении
+              />
+              {fieldState.error && <ErrorText text={fieldState.error.message} />}
+            </>
+          )}
+        />
 
         <FormTextarea
           name="comment"
