6561d71c18d829edc38a7b2b69480d625c871549
Raman Andreyuk <romaandreyuk@gmail.com>
2025-10-09 14:31:46 +0200

    added import

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import { Header } from '@/shared/components/shared/header';
 import { Container } from '@/shared/components/shared/container';
+import React from 'react';
 
 export const metadata: Metadata = {
   title: 'Next Pizza | Корзина',
