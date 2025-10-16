b566addb93b4cc393474f0ed3c3d539542952844
Raman Andreyuk <romaandreyuk@gmail.com>
2025-10-17 00:02:09 +0200

    react-insta-stories. schema.prisma story & storyItem, api/stories/route.ts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import { Filters } from '@/shared/components/shared/filters';
 import { ProductsGroupList } from '@/shared/components/shared/products-group-list';
 import { Suspense } from 'react';
 import { findPizzas, GetSearchParams } from '@/shared/lib/find-pizzas';
+import { Stories } from '@/shared/components/shared/stories';
 
 export default async function Home({ searchParams }: { searchParams: GetSearchParams }) {
   const categories = await findPizzas(searchParams);
@@ -15,7 +16,7 @@ export default async function Home({ searchParams }: { searchParams: GetSearchPa
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
       <TopBar categories={categories.filter((category) => category.products.length > 0)} />
-
+      <Stories />
       <Container className=" mt-10 pb-14">
         <div className="flex gap-[80px]">
           {/* Фильтрация */}
